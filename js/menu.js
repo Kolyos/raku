7c26a8a243be3226b7c3f53f39f5af710ccc6d51
Kolyos <105753490+Kolyos@users.noreply.github.com>
2023-04-07 16:52:50 +0300

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,89 @@
+const menuZama = document.querySelector('.menu__zama');
+const zamaImg = document.querySelector('.zama__img');
+if(menuZama){
+   
+   menuZama.addEventListener("click", function(e){
+      menuZama.classList.toggle('_active');
+      zamaImg.classList.toggle('_active');
+})};
+
+const menuBor = document.querySelector('.menu__redbor');
+const BorImg = document.querySelector('.redbor__img');
+if(menuBor){
+   
+   menuBor.addEventListener("click", function(e){
+      menuBor.classList.toggle('_active');
+      BorImg.classList.toggle('_active');
+})};
+
+const menuSol = document.querySelector('.menu__solyan');
+const SolImg = document.querySelector('.solyan__img');
+if(menuSol){
+   
+   menuSol.addEventListener("click", function(e){
+      menuSol.classList.toggle('_active');
+      SolImg.classList.toggle('_active');
+})};
+
+const menuWurpa = document.querySelector('.menu__wurpa');
+const wurpaImg = document.querySelector('.wurpa__img');
+if(menuWurpa){
+   
+   menuWurpa.addEventListener("click", function(e){
+      menuWurpa.classList.toggle('_active');
+      wurpaImg.classList.toggle('_active');
+})};
+
+const menuOtbivSwine = document.querySelector('.menu_otbivswine');
+const otbivSwineImg = document.querySelector('.orbivSwine__img');
+if(menuOtbivSwine){
+   
+   menuOtbivSwine.addEventListener("click", function(e){
+      menuOtbivSwine.classList.toggle('_active');
+      otbivSwineImg.classList.toggle('_active');
+})};
+
+const menuBones = document.querySelector('.menu_bones');
+const bonesSwineImg = document.querySelector('.bones__img');
+if(menuBones){
+   
+   menuBones.addEventListener("click", function(e){
+      menuBones.classList.toggle('_active');
+      bonesSwineImg.classList.toggle('_active');
+})};
+
+const menuTokana = document.querySelector('.menu__tokana');
+const tokanaImg = document.querySelector('.tokana__img');
+if(menuTokana){
+   
+   menuTokana.addEventListener("click", function(e){
+      menuTokana.classList.toggle('_active');
+      tokanaImg.classList.toggle('_active');
+})};
+
+const menuKurFile = document.querySelector('.menu__otbivfil');
+const kurFileImg = document.querySelector('.otbivfil__img');
+if(menuKurFile){
+   
+   menuKurFile.addEventListener("click", function(e){
+      menuKurFile.classList.toggle('_active');
+      kurFileImg.classList.toggle('_active');
+})};
+
+const menuOtbiOvow = document.querySelector('.menu__otbiovow');
+const otbiOvowFileImg = document.querySelector('.otbiovow__img');
+if(menuOtbiOvow){
+   
+   menuOtbiOvow.addEventListener("click", function(e){
+      menuOtbiOvow.classList.toggle('_active');
+      otbiOvowFileImg.classList.toggle('_active');
+})};
+
+const menuOtbivGrob = document.querySelector('.menu__otbigrob');
+const otbivGrobImg = document.querySelector('.otbigrob__img');
+if(menuOtbivGrob){
+   
+   menuOtbivGrob.addEventListener("click", function(e){
+      menuOtbivGrob.classList.toggle('_active');
+      otbivGrobImg.classList.toggle('_active');
+})};
